068449b23d78590ee3386cbb6d41970531277007
Varun Rau <varunrau@gmail.com>
2020-08-15 15:36:36 -0700

    add hour selection option

--- app.js
+++ app.js
@@ -4,21 +4,44 @@ const { ipcRenderer } = require("electron");
 document.addEventListener("DOMContentLoaded", () => {
   const dateSelect = document.getElementById("date");
 
-  const dates = Array(8).fill().map((_, i) => {
-    const d = moment().add(7 - i, 'days').format('MM-DD-YYYY');
-    const option = document.createElement("option");
-    option.text = d;
-    dateSelect.add(option);
-  });
+  const dates = Array(8)
+    .fill()
+    .map((_, i) => {
+      const d = moment()
+        .add(7 - i, "days")
+        .format("MM-DD-YYYY");
+      const option = document.createElement("option");
+      option.text = d;
+      dateSelect.add(option);
+    });
+
+  // 10:00am - 3:00pm
+  const hourSelect = document.getElementById("hour");
+  const hours = Array(10)
+    .fill()
+    .map((_, i) => {
+      let hour = (10 + i) % 12;
+      hour = hour === 0 ? 12 : hour;
+      const option = document.createElement("option");
+      option.text = `${hour}:00${[10, 11].includes(hour) ? "am" : "pm"}`;
+      hourSelect.add(option);
+
+      const optionHalf = document.createElement("option");
+      optionHalf.text = `${hour}:30${[10, 11].includes(hour) ? "am" : "pm"}`;
+      hourSelect.add(optionHalf);
+    });
+  hourSelect.value = "11:00am";
 
   document.getElementById("start-btn").addEventListener("click", () => {
     const date = document.getElementById("date").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    ipcRenderer.sendSync('run-puppeteer', {
-      email, password, date
+    const hour = document.getElementById("hour").value;
+    ipcRenderer.sendSync("run-puppeteer", {
+      email,
+      password,
+      date,
+      hour,
     });
   });
 });
-
-
--- run.js
+++ run.js
@@ -1,72 +1,90 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const moment = require("moment");
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function run(email, password, date) {
-
+async function run(email, password, date, hour) {
   const browser = await puppeteer.launch({
     headless: false,
   });
 
   // Log in for torrey pines
   const page = await browser.newPage();
-  await page.goto('https://foreupsoftware.com/index.php/booking/19347/1468', {waitUntil: 'networkidle2'});
-  await page.type('#login_email', email)
-  await page.type('#login_password', password)
+  await page.goto("https://foreupsoftware.com/index.php/booking/19347/1468", {
+    waitUntil: "networkidle2",
+  });
+  await page.type("#login_email", email);
+  await page.type("#login_password", password);
   await Promise.all([
-    page.waitForNavigation({waitUntil: 'networkidle2'}),
-    page.click('#submit_button', {delay: 500}),
+    page.waitForNavigation({ waitUntil: "networkidle2" }),
+    page.click("#submit_button", { delay: 500 }),
   ]);
-  console.log("logging into account")
+  console.log("logging into account");
 
-  await page.goto('https://foreupsoftware.com/index.php/booking/19347/1468#/teetimes', {waitUntil: 'networkidle2'});
+  await page.goto(
+    "https://foreupsoftware.com/index.php/booking/19347/1468#/teetimes",
+    { waitUntil: "networkidle2" }
+  );
 
-  console.log("going to page")
+  console.log("going to page");
 
   await page.waitFor(5000);
-  await page.click('.booking-classes button:nth-child(4)', {delay: 500})
+  await page.click(".booking-classes button:nth-child(4)", { delay: 500 });
 
-  await sleep(5000)
-  console.log("waited for 5 seconds")
+  await sleep(5000);
+  console.log("waited for 5 seconds");
 
-  console.log("on the teesheet page")
+  console.log("on the teesheet page");
 
   // select 4 players
-  await page.click('.players :nth-child(4)')
-  console.log('selected 4 players')
-
-  await page.click('.time :nth-child(2)')
-  console.log('selected midday')
+  await page.click(".players :nth-child(4)");
+  console.log("selected 4 players");
 
   // select date
-  await page.select('select#date-menu', date)
-  console.log('selected date')
+  await page.select("select#date-menu", date);
+  console.log("selected date");
 
-  await page.click('.holes :nth-child(1)', {delay: 500})
+  await page.click(".holes :nth-child(1)", { delay: 500 });
 
-  let teeTimes = await page.evaluate(() => {
-    const teeTimes = document.querySelectorAll(".reserve-time .start")
-    console.log('tee times on page', teeTimes)
-    return Array.from(teeTimes, el => el.innerText)
-  })
-  console.log('tee times', teeTimes)
+  let teeTimes = [];
+  let teeTimeIndex = -1;
 
-  while (teeTimes.length == 0) {
-    console.log("didn't find any tee times - waiting for one second")
-    await sleep(200);
-    await page.click('.holes :nth-child(1)', {delay: 500})
+  do {
+    teeTimeIndex = await page.evaluate((hour) => {
+      const teeTimes = document.querySelectorAll(".reserve-time .start");
+      const times = Array.from(teeTimes, (el) => el.innerText);
+      console.log("times", times);
+      const filteredTimes = times.filter((time) => {
+        return moment(time, "H:mm") >= moment(hour, "H:mm");
+      });
+      console.log("filteredTimes", filteredTimes);
+      if (filteredTimes.length > 0) {
+        return times.indexOf(filteredTimes[0]);
+      }
+      if (times.length > 0) {
+        return 0;
+      }
+      console.log("didn't find any tee times - waiting for 300 milliseconds");
+      return -1;
+    }, hour);
+    await Promise.all([sleep(300), page.click(".holes :nth-child(1)")]);
+  } while (teeTimeIndex === -1);
 
-    teeTimes = await page.evaluate(() => {
-      const teeTimes = document.querySelectorAll(".reserve-time .start")
-      console.log('tee times on page', teeTimes)
-      return Array.from(teeTimes, el => el.innerText)
-    })
+  console.log("index", teeTimeIndex);
+
+  await page.click(`#times li:nth-child(${teeTimeIndex + 1})`);
+
+  while (true) {
+    try {
+      await Promise.all([
+        sleep(100),
+        page.click(`#times li:nth-child(${teeTimeIndex + 1})`),
+      ]);
+    } catch {}
   }
-
-  await page.click('.reserve-time .start')
-};
+}
 
 module.exports = {
   run: run,
